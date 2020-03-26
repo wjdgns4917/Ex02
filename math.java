1974a6c928dab044ac10b250c034a42d8963739a
wjdgns4917 <hoony0802@naver.com>
2020-03-26 16:35:59 +0900

    math01

---
+++ math.java
@@ -0,0 +1,20 @@
+public class math{
+	public static void main(String [] args){
+		int width=100;
+		int height=200;
+		double result=0.0;
+		result=width*height/2;
+
+		System.out.println("triangle area : "+result);
+		System.out.println("square area : "+width*height);		
+		int r=10;
+		double p=3.14;
+
+		System.out.println("circle area : "+r*r*p);
+
+	
+
+		System.out.println("cylindrical volume : "+r*r*p*height);
+	}
+}
+
